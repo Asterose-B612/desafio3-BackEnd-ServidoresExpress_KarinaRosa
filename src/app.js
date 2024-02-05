86747fcee8815edf616838d8f5d4b72d4cc33f11
Karina Rosa <decodikiaro@gmail.com>
2024-02-05 11:30:47 -0300

    Generación automática de dependencias express después de su instalación.  Modificación importación express. Implementacion de metodos, definición de rutas products.

--- src/app.js
+++ src/app.js
@@ -1,28 +1,48 @@
-//npm run nodemon
+//EN CONSOLA: CORRER PUERTO: npm run nodemon o npm devNode
+
+//importo modulo express para crear el servidor
+import express from 'express'
 
 
-//importo modulo http para crear el servidor
-import http from 'http'
-import nodemon from 'nodemon'
+//hago referencia a lo q seria todo el codigo de express
+const APP = express()
+//variable para cambios de puertos
+const PORT = 8000
 
-//SINTAXIS: Creo un servidor que me va a devolver una respuesta simple con createServer y les paso los parametros req (donde consulto) y res (donde respondo), funcion flecha
-//req = request = una peticion
-//res = response = una respuesta
-const app = http.createServer((req,res)=>{
+//en la ruta inicial ejecitame el siguiente comando
+APP.get('/', (req, res) => {
 
-res.end("hola este es mi primer servidor con express")
+    res.send("Servidor creado recientemente en Express")
 
 })
-//metodo para finalizar una peticion, es decir enviar una info, en lo que seria un res
 
-/*todos los servidores debe estar alojado en algun lugar. Voy a tener q tener un puerto de lo que seria mi computadora activo para poder enviar peticiones a estos elementos
-*/
 
-//Defino mi servidor app, y va a escuchar el puerto de mi aplicacion que voy a habilitar para que se pueda comunicar con el mundo externo en este caso lo que seria mi propia computadora
-//defino el puerto 8000, funcion anonimay le digo que cuando me pueda conectar a este puerto, me devuelva un mensaje
+// Ruta productos recibe 1 límite opcional de cantidad.
+// IF proporciona → limita cantidad productos, ELSE se envían todos.
+APP.get(`/products`, (req, res) => {
+    
+    //CONSULTAR EL ARRAY DE PRODS QUE TRAIGO DESDE FILE SYSTEM:consulto mis querys, el elemento limit.
+    const { limit } = req.params.query
 
-app.listen(8000,()=>{
+})
 
-    console.log("Server on port 8000")
+
+
+//consulta en ruta productos. pid no es valor fijo.
+APP.get(`/products/:pid`, (req, res) => {
+    //consulto por parametro la solicitud
+    const PRODUCTID = req.params.pid
+    //recibo el id y lo muestro en consola 
+    console.log(PRODUCTID)
+    //llamo a ProductManager para devolver prod c/id solicitado
+    res.send(`Id: ${PRODUCTID}`)
+
+})
+
+
+//Defino mi servidor → APP, APP escucha PORT, f anonima → show message
+APP.listen(PORT, () => {
+
+    console.log(`Server on port ${PORT}`)
 
 })
